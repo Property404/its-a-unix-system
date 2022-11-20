a9994084d53fc87fa317891874ad8d06a25ce900
Property404 <dagan@dagans.dev>
2022-11-20 16:49:32 -0500

    Allow backspace

--- src/streams/standard_streams.rs
+++ src/streams/standard_streams.rs
@@ -1,26 +1,33 @@
-use crate::streams::{Backend, InputStream, OutputStream, TerminalReader, TerminalWriter};
-use crate::utils;
-use anyhow::{anyhow, bail, Result};
+use crate::{
+    streams::{Backend, InputStream, OutputStream, TerminalReader, TerminalWriter},
+    utils,
+};
+use anyhow::{anyhow, Result};
 use futures::{
     channel::mpsc::{self, UnboundedReceiver},
     stream::{FusedStream, Stream},
 };
 use std::{
-    io::Write,
     pin::Pin,
     task::{Context, Poll},
 };
-use wasm_bindgen::{closure::Closure, JsCast};
-use web_sys::{self, Element, KeyboardEvent};
+use wasm_bindgen::{closure::Closure, prelude::*, JsCast};
+use web_sys::{self, KeyboardEvent};
+
+#[wasm_bindgen]
+extern "C" {
+    fn js_term_write(s: &str);
+    fn js_term_backspace();
+}
 
 pub fn standard() -> Result<(
     InputStream,
     OutputStream,
     Backend<KeyboardTerminalReader, HtmlTerminalWriter>,
 )> {
-    let writer = HtmlTerminalWriter::new()?;
+    let writer = HtmlTerminalWriter::default();
     let (output_stream, output_bkend) = OutputStream::from_writer(writer);
-    let reader = KeyboardTerminalReader::new(output_stream.clone())?;
+    let reader = KeyboardTerminalReader::new()?;
     let (input_stream, input_bkend) = InputStream::from_reader(reader);
 
     let backend = Backend {
@@ -55,7 +62,7 @@ impl FusedStream for KeyboardTerminalReader {
 // This is a "Sit Still and Look Pretty" struct.
 // Just existing should be enough for it to...do things.
 impl KeyboardTerminalReader {
-    fn new(mut out: OutputStream) -> Result<KeyboardTerminalReader> {
+    fn new() -> Result<KeyboardTerminalReader> {
         let document = utils::get_document()?;
         let (sender, receiver) = mpsc::unbounded();
         let mut cbuffer = Vec::<u8>::new();
@@ -63,16 +70,13 @@ impl KeyboardTerminalReader {
         let callback = Closure::new(move |e: KeyboardEvent| {
             let key = e.key();
             if key.len() == 1 {
+                js_term_write(&key);
+                cbuffer.extend(key.as_bytes());
                 if "'/?".contains(&key) {
                     e.prevent_default();
                 }
-                let key: Vec<u8> = key.as_bytes().into();
-                out.write_all(&key).expect("Failed to write");
-                out.flush().expect("Failed to flush");
-                cbuffer.extend(&key);
             } else if key == "Enter" {
-                out.write_all(b"\n").expect("Failed to write");
-                out.flush().expect("Failed to flush");
+                js_term_write("\n");
                 cbuffer.push(b'\n');
 
                 sender
@@ -80,6 +84,9 @@ impl KeyboardTerminalReader {
                     .expect("Send failed :(");
 
                 cbuffer.clear();
+            } else if key == "Backspace" && !cbuffer.is_empty() {
+                js_term_backspace();
+                cbuffer.pop();
             }
         });
         document
@@ -103,23 +110,12 @@ impl Drop for KeyboardTerminalReader {
     }
 }
 
-pub struct HtmlTerminalWriter {
-    root: Element,
-}
-
-impl HtmlTerminalWriter {
-    fn new() -> Result<Self> {
-        let Some(root) = utils::get_document()?.get_element_by_id("terminal") else {
-            bail!("Could not find terminal element");
-        };
-        Ok(Self { root })
-    }
-}
+#[derive(Default, Clone)]
+pub struct HtmlTerminalWriter {}
 
 impl TerminalWriter for HtmlTerminalWriter {
     fn send(&mut self, content: &str) -> Result<()> {
-        let content = self.root.text_content().unwrap_or_default() + content;
-        self.root.set_text_content(Some(&content));
+        js_term_write(content);
         Ok(())
     }
 }
--- www/utils.js
+++ www/utils.js
@@ -1,7 +1,14 @@
-export function waste_time() {
-    return new Promise((resolve,reject) => {
-        setTimeout(()=> {
-            resolve()
-        }, 3000);
-    });
+const terminal = document.getElementById("terminal");
+
+function js_term_write(str) {
+    terminal.textContent += str;
+}
+
+function js_term_clear() {
+    terminal.textContent = "";
+}
+
+function js_term_backspace() {
+    terminal.textContent =
+        terminal.textContent.substr(0, terminal.textContent.length - 1);
 }
