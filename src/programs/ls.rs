53932b335997292b0cae378f84ec7970bc4773e7
Property404 <dagan@dagans.dev>
2022-11-22 15:48:58 -0500

    Fix ls

--- src/programs/ls.rs
+++ src/programs/ls.rs
@@ -1,18 +1,24 @@
 use crate::process::Process;
-use anyhow::Result;
+use anyhow::{bail, Result};
 use std::io::Write;
 
 pub async fn ls(process: &mut Process, args: Vec<String>) -> Result<()> {
-    let directories = if args.len() == 1 {
-        process.cwd.walk_dir()?
+    let path = if args.len() == 1 {
+        process.cwd.clone()
     } else {
         let dir = process.cwd.join(&args[1])?;
-        dir.walk_dir()?
+        if !dir.exists()? {
+            bail!("ls: no such file or directory: {}", dir.as_str());
+        }
+        if !dir.is_dir()? {
+            bail!("ls: not a directory: {}", dir.as_str());
+        }
+        dir
     };
 
-    for entity in directories {
+    for entity in path.walk_dir()? {
         let entity = entity?;
-        if entity.parent().as_ref() != Some(&process.cwd) {
+        if entity.parent().as_ref() != Some(&path) {
             break;
         }
         let display = format!("{}\n", entity.filename());
