07e1d0980b169cdec9295808db97bb3fce0117c6
Property404 <dagan@dagans.dev>
2022-11-22 18:31:21 -0500

    Remove wee alloc

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,6 @@ use futures::try_join;
 use process::Process;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 async fn run() -> Result<()> {
     utils::set_panic_hook();
 
