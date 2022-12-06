ed1d400e6255cccc9edcd0c0578f819859770e30
Property404 <martinez.dagan@gmail.com>
2022-12-06 18:19:49 -0500

    Add easter egg

--- src/programs/touch.rs
+++ src/programs/touch.rs
@@ -1,6 +1,7 @@
 use crate::process::Process;
 use anyhow::Result;
 use clap::Parser;
+use futures::io::AsyncWriteExt;
 
 /// Create a file if it does not exist.
 #[derive(Parser)]
@@ -13,6 +14,10 @@ struct Options {
 pub async fn touch(process: &mut Process, args: Vec<String>) -> Result<()> {
     let options = Options::try_parse_from(args.into_iter())?;
     for arg in options.files.into_iter() {
+        if arg == "me" {
+            process.stderr.write_all(
+            b"Absolutely not.\n").await?;
+        }
         process.get_path(arg)?.create_file()?;
     }
     Ok(())
