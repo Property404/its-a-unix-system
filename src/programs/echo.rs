38b48bffab4418651b08eb52e1e209207381724b
Property404 <11879454+Property404@users.noreply.github.com>
2022-12-09 23:10:05 -0500

    Make `echo` behave more like original program

--- src/programs/echo.rs
+++ src/programs/echo.rs
@@ -16,15 +16,22 @@ struct Options {
 pub async fn echo(process: &mut Process) -> Result<()> {
     let options = Options::try_parse_from(process.args.iter())?;
     let mut args = options.args.into_iter();
+    let mut ends_with_line_feed = false;
+
     if let Some(first_argument) = args.next() {
         process.stdout.write_all(first_argument.as_bytes()).await?;
+        ends_with_line_feed = first_argument.ends_with('\n');
     }
     for item in args {
         process.stdout.write_all(b" ").await?;
         process.stdout.write_all(item.as_bytes()).await?;
+        ends_with_line_feed = item.ends_with('\n');
     }
 
-    if !options.no_newline {
+    // echo on Linux seems to not print an extra newline if stuff-to-be-echoed ends with a newline
+    // already. Which kind of makes sense from a usabilty perspective.
+    // Let's emulate that.
+    if !options.no_newline && !ends_with_line_feed {
         process.stdout.write_all(b"\n").await?;
     }
 
