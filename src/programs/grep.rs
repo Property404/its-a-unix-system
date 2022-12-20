8798c4c62afa975d3560c65bdfb70a9b5273aa99
Property404 <11879454+Property404@users.noreply.github.com>
2022-12-19 22:50:25 -0500

    Add --invert-match option to grep

--- src/programs/grep.rs
+++ src/programs/grep.rs
@@ -15,15 +15,19 @@ struct Options {
     pattern: String,
     /// The files to filter.
     files: Vec<String>,
+    /// Select non-matching lines.
+    #[arg(short = 'v', long)]
+    invert_match: bool,
 }
 
 pub async fn grep_inner(
     stream: &mut InputStream,
     out: &mut OutputStream,
     pattern: &Regex,
+    invert: bool,
 ) -> Result<()> {
     while let Ok(line) = stream.get_line().await {
-        if pattern.is_match(&line) {
+        if pattern.is_match(&line) != invert {
             out.write_all(line.as_bytes())?;
             out.write_all(b"\n")?;
         }
@@ -38,14 +42,20 @@ pub async fn grep(process: &mut Process) -> Result<ExitCode> {
 
     if options.files.is_empty() {
         let mut stdin = process.stdin.clone();
-        grep_inner(&mut stdin, &mut process.stdout, &pattern).await?;
+        grep_inner(
+            &mut stdin,
+            &mut process.stdout,
+            &pattern,
+            options.invert_match,
+        )
+        .await?;
     } else {
         for file in options.files {
             let fp = process.get_path(file)?.open_file()?;
             let (mut fp, mut backend) = file_redirect_in(Box::new(fp));
             try_join! {
                 async {
-                    grep_inner(&mut fp, &mut process.stdout, &pattern).await?;
+                    grep_inner(&mut fp, &mut process.stdout, &pattern, options.invert_match).await?;
                     fp.shutdown().await?;
                     Ok(())
                 },
